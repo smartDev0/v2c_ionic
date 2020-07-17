a6b3b37856d72108a6dd009cff3cc1f7c6b9fb8a
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-17 17:31:23 +0800

    add some page fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
   today;
+  maxPowverValue;
+  currentPowverVale;
+  maxCostValue = 200;
+  percentPower: Number;
+  currentCostVale = 72.8;
+  percentCost: Number;
   constructor() {
     this.today = Date.now();
+    this.currentPowverVale = 280;
+    this.maxPowverValue = 2000;
+    this.percentPower = Math.round(
+      (this.currentPowverVale / this.maxPowverValue) * 100
+    );
+    this.percentCost = Math.round(
+      (this.currentCostVale / this.maxCostValue) * 100
+    );
   }
 
   ngOnInit() {}
+  ngAfterViewInit() {}
 }
