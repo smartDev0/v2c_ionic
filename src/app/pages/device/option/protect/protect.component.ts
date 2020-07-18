50801779ec1b676a4d12405ad0bf8f9f13f5b40a
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-18 15:28:08 +0100

    remove console.log

--- src/app/pages/device/option/protect/protect.component.ts
+++ src/app/pages/device/option/protect/protect.component.ts
@@ -25,7 +25,6 @@ export class ProtectComponent implements OnInit {
     }
   }
   radioOnChecked(event) {
-    console.log(this.ionRadioValue);
     if (event.target.value != "Fotovoltaica") {
       this.ionRadioSubValue = "";
     } else {
