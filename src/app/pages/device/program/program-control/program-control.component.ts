c44d4d4248628e5ef72b24c1933ac769f34cb8ae
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-18 04:22:28 +0100

    add 95 % done

--- src/app/pages/device/program/program-control/program-control.component.ts
+++ src/app/pages/device/program/program-control/program-control.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonInput } from "@ionic/angular";
 
 @Component({
   selector: "app-program-control",
@@ -7,10 +8,19 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ProgramControlComponent implements OnInit {
   segment = 1;
+  // @ViewChild("num1", { static: false }) num1Ref: any;
+  // @ViewChild("num2", { static: false }) num2Ref: any;
+  // @ViewChild("num3", { static: false }) num3Ref: any;
+  // @ViewChild("num4", { static: false }) num4Ref: any;
+
   constructor() {}
 
   ngOnInit() {}
   segmentChanged(event) {
     this.segment = Number(event);
+  }
+
+  moveFocus(event, nextElement) {
+    if (event.target.value != "") nextElement.focus();
   }
 }
