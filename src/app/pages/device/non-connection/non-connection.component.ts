aae86b7f5d8ce0afa12c9b0a000878b7f3aa11e6
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-24 20:39:46 +0100

    add change ion-range

--- src/app/pages/device/non-connection/non-connection.component.ts
+++ src/app/pages/device/non-connection/non-connection.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./non-connection.component.scss"],
 })
 export class NonConnectionComponent implements OnInit {
+  color: string;
+  number: number;
   constructor() {}
 
   ngOnInit() {}
   segmentChanged(event) {}
+  onRangeChangeHandler() {
+    // if (this.number > 0 && this.number < 26) {
+    //   this.color = "dark";
+    // } else if (this.number > 25 && this.number < 51) {
+    //   this.color = "primary";
+    // } else if (this.number > 50 && this.number < 76) {
+    //   this.color = "secondary";
+    // } else {
+    //   this.color = "danger";
+    // }
+  }
 }
