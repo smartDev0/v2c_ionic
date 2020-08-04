92f08ffa6de5f86ff1a851006192cf2fdcb511a4
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-08-04 03:26:16 +0100

    add active stats on pairing page

--- src/app/pages/charger/charger/charger.component.ts
+++ src/app/pages/charger/charger/charger.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./charger.component.scss"],
 })
 export class ChargerComponent implements OnInit {
+  public isActiveOne = false;
+  public isActiveTwo = false;
+  public isActiveThree = false;
   BannerOption = {
     initialSlide: 0,
     slidesPerView: 3,
@@ -18,4 +21,20 @@ export class ChargerComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  onChangeStats(event) {
+    console.log(event);
+    if (event == 1) {
+      this.isActiveOne = !this.isActiveOne;
+      this.isActiveTwo = false;
+      this.isActiveThree = false;
+    } else if (event == 2) {
+      this.isActiveOne = false;
+      this.isActiveTwo = !this.isActiveTwo;
+      this.isActiveThree = false;
+    } else if (event == 3) {
+      this.isActiveThree = !this.isActiveThree;
+      this.isActiveOne = false;
+      this.isActiveTwo = false;
+    }
+  }
 }
